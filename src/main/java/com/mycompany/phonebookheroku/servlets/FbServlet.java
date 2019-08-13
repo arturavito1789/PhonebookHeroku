ee2f15ff760233f0a064b8e2b04dcc0ecc78544c
arturavito1789 <arturavito1789@gmail.com>
2019-08-13 14:15:23 +0300

    g

--- src/main/java/com/mycompany/phonebookheroku/servlets/FbServlet.java
+++ src/main/java/com/mycompany/phonebookheroku/servlets/FbServlet.java
@@ -10,12 +10,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@MultipartConfig
 @WebServlet(name = "FbServlet", urlPatterns = {"/FbServlet"})
 public class FbServlet extends HttpServlet {
 
